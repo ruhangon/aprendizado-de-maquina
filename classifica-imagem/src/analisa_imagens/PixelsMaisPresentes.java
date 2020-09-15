d39558a499e62af393bef38ff62e5b2859e212c2
Ruhan Gonçalves <ruhan.goncalves@gmail.com>
2020-09-15 02:06:57 -0300

    Altera nomes de variáveis para facilitar entendimento do código

--- classifica-imagem/src/analisa_imagens/PixelsMaisPresentes.java
+++ classifica-imagem/src/analisa_imagens/PixelsMaisPresentes.java
@@ -5,18 +5,18 @@ public class PixelsMaisPresentes {
 	 * int[][][] contaPixels armazenará a contagem de cada pixel, primeiro se refere
 	 * ao red, segundo green e terceiro blue. A cada vez que passar por um pixel
 	 * novo, já irá comparar para saber se ele pega o lugar de um dos três que estão
-	 * salvos como mais presentes
+	 * salvos como pixels presentes
 	 */
 	private int[][][] contaPixels;
-	private int[] red;
-	private int[] green;
-	private int[] blue;
+	private int[] pixelPresente1; // se refere ao pixel mais presente nas imagens analisadas
+	private int[] pixelPresente2; // se refere ao décimo pixel mais presente nas imagens analisadas
+	private int[] pixelPresente3; // se refere ao vigésimo pixel mais presente nas imagens analisadas
 
 	public PixelsMaisPresentes() {
 		contaPixels = new int[256][256][256];
-		red = new int[3];
-		green = new int[3];
-		blue = new int[3];
+		pixelPresente1 = new int[3];
+		pixelPresente2 = new int[3];
+		pixelPresente3 = new int[3];
 	}
 
 	public int[][][] getContaPixels() {
@@ -27,28 +27,28 @@ public class PixelsMaisPresentes {
 		this.contaPixels = contaPixels;
 	}
 
-	public int[] getRed() {
-		return red;
+	public int[] getPixelPresente1() {
+		return pixelPresente1;
 	}
 
-	public void setRed(int[] red) {
-		this.red = red;
+	public void setPixelPresente1(int[] pixelPresente1) {
+		this.pixelPresente1 = pixelPresente1;
 	}
 
-	public int[] getGreen() {
-		return green;
+	public int[] getPixelPresente2() {
+		return pixelPresente2;
 	}
 
-	public void setGreen(int[] green) {
-		this.green = green;
+	public void setPixelPresente2(int[] pixelPresente2) {
+		this.pixelPresente2 = pixelPresente2;
 	}
 
-	public int[] getBlue() {
-		return blue;
+	public int[] getPixelPresente3() {
+		return pixelPresente3;
 	}
 
-	public void setBlue(int[] blue) {
-		this.blue = blue;
+	public void setPixelPresente3(int[] pixelPresente3) {
+		this.pixelPresente3 = pixelPresente3;
 	}
 
 }
